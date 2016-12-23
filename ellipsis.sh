2c4f9c80b918b3927d12a8491b0bc9f2efc897e8
Torgny Bjers <torgny.bjers@gmail.com>
2016-12-22 23:05:56 -0500

    Added ellipsis file

---
+++ ellipsis.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+#
+# tbjers/dot-elixir ellipsis package
+
+pkg.install() {
+  git.clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.2.0
+  case $(os.platform) in
+    osx)
+      if utils.cmd_exists brew; then
+        brew install automake autoconf openssl libyaml readline libxslt libtool unixodbc
+      fi
+      ;;
+    linux)
+      if utils.cmd_exists apt-get; then
+        sudo apt-get install automake autoconf libreadline-dev libncurses-dev libssl-dev libyaml-dev libxslt-dev libffi-dev libtool unixodbc-dev m4
+      fi
+      ;;
+  esac
+  . $HOME/.asdf/asdf.sh
+  asdf plugin-add erlang https://github.com/asdf-vm/asdf-erlang.git
+  asdf install erlang 19
+}
